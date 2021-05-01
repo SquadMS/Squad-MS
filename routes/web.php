2e5c5f0a72e248c7d9a2c2a864dbed06f1d8a114
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-01 09:15:38 +0200

    Remove unecessary routes, Update SquadMS foundation, Load routes from SquadMSRouter,

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use SquadMS\Foundation\SquadMSRouter;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,3 +13,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+SquadMSRouter::getInstance()->register();
