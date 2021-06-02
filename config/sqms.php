692ff34dd31e22b341712377cdc4d21de531745c
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-02 07:41:45 +0200

    Fix auth config, Update dependencies, RCON Worker can connect,

--- config/sqms.php
+++ config/sqms.php
@@ -6,6 +6,18 @@ return [
     ],
 
     'auth' => [
-        'redirect' => 1,
+        'redirect' => 0, // 0 = Redirect Home, 1 = Redirect previous page, 3 = Redirect Profile
+
+        'controller' => \SquadMS\Foundation\Auth\Http\Controllers\SteamLoginController::class,
+
+        'api_key' => env('STEAM_API_KEY'),
+
+        /*
+         * Method of retrieving player's profile data.
+         * Valid options: xml, api
+         */
+        'method'  => env('STEAM_LOGIN_PROFILE_DATA_METHOD', env('STEAM_LOGIN_PROFILE_METHOD', 'api')),
+
+        'timeout' => env('STEAM_LOGIN_TIMEOUT', 5),
     ],
 ];
