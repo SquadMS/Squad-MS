e83568954ccb6cb8558685b6cd0c7b2c12cb9b35
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-18 00:52:35 +0200

    Add Servers User Trait, Update dependencies,

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Support\Facades\Auth;
 use SquadMS\Foundation\Contracts\SquadMSUser;
+use SquadMS\Servers\Contracts\SquadMSServersUserInterface;
+use SquadMS\Servers\Traits\SquadMSServersUserTrait;
 
-class User extends SquadMSUser
+class User extends SquadMSUser implements SquadMSServersUserInterface
 {
+    use SquadMSServersUserTrait;
+    
     public static function current() : ?self
     {
         return Auth::user();
