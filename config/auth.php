20a79fbcaa608a8a75aa6bfbd097f8f006c6e321
Patrick <privat@skyraptor.eu>
2022-06-09 21:46:37 +0200

    Change: Use sqms foundation model for auth, Remove: Delete User model,

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => SquadMS\Foundation\Models\SquadMSUser::class,
         ],
 
         // 'users' => [
