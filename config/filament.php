249bbca82be4cfd21ee51c69588c3e68615cd230
Patrick <privat@skyraptor.eu>
2022-06-11 19:00:11 +0200

    Change: Use filament deps, Change: Allow dark mode,

---
+++ config/filament.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Dark mode
+    |--------------------------------------------------------------------------
+    |
+    | By enabling this feature, your users are able to select between a light
+    | and dark appearance for the admin panel, or let their system decide.
+    |
+    */
+
+    'dark_mode' => true,
+];
