de6175e91d530c60cbbf7a49a911889117abc27a
Patrick <privat@skyraptor.eu>
2022-06-12 12:04:33 +0200

    Add: SquadMS Rules and configure spatie translateable plugin,

---
+++ config/filament-spatie-laravel-translatable-plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Locales
+    |--------------------------------------------------------------------------
+    |
+    | These are the locales that Filament will use to put translate resource
+    | content. They may be overridden for each resource by setting the
+    | `$translatableLocales` property.
+    |
+    */
+
+    'default_locales' => [
+        'en',
+        'de',
+        'ar',
+        'he',
+    ],
+
+];
