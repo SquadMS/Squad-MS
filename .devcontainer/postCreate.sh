136582b20bcc08cada8a82a9daf888782facc260
Patrick <patrick.henninger@contagt.com>
2022-06-30 17:55:46 +0000

    Fix: use onCreate instead of initialize

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+# Migrate the database
+./artisan migrate
+
 # Install Packagist dependencies and run scripts
 composer install
 
 # Install NPM dependencies and build assets
 npm install
 npm run dev
-
-# Migrate the database
-./artisan migrate
