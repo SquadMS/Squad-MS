01c415e2c6b610092ece8e4bc3d64bf0c62b1ef0
Patrick <patrick.henninger@contagt.com>
2022-06-30 17:22:31 +0000

    Fix: init script not setting APP_KEY correctly, Change: init script enable xDebug

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -6,16 +6,12 @@ then
     cp .env.example .env
     
     # Set APP_KEY
-    sed "s/^APP_KEY=/APP_KEY=base64:ZEKyyTRYfKC9pYEQuxMEDA4PN6qwxyyhkOibrOMr1s4=/" .env
+    sed -i "s/^APP_KEY=/APP_KEY=base64:ZEKyyTRYfKC9pYEQuxMEDA4PN6qwxyyhkOibrOMr1s4=/" .env
+
+    # Enable xDebug
+    sed -i "s/^SAIL_XDEBUG_MODE=/SAIL_XDEBUG_MODE=debug,develop,profile/" .env
     
     # Remove blank STEAM_API_KEY (Use GitHub Secrets)
-    sed '/^STEAM_API_KEY=/d' .env
+    sed -i '/^STEAM_API_KEY=/d' .env
 fi
 
-# Install Packagist dependencies (without post-* scripts)
-docker run --rm \
-    -u "$(id -u):$(id -g)" \
-    -v $(pwd):/var/www/html \
-    -w /var/www/html \
-    laravelsail/php81-composer:latest \
-    composer install --no-scripts --ignore-platform-reqs
