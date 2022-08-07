d67d351b09f5f953957100b269a3755638f2d662
Patrick <privat@skyraptor.eu>
2022-08-07 12:34:03 +0200

    Configure filament in app, Change worker port, Fix worker startup, Updated assets,

---
+++ config/filament.php
@@ -0,0 +1,12 @@
+<?php
+
+use SquadMS\Foundation\Http\Livewire\Auth\Login;
+
+return [
+    'dark_mode' => true,
+    'auth' => [
+        'pages' => [
+            'login' => Login::class
+        ]
+    ]
+];
